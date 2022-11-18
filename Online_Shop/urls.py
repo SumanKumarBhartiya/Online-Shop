2f57f37b56d9269845ce7fa4a3ba1509eebb2c77
Suman Kumar Bhartiya <skb08725@gmail.com>
2022-11-18 18:00:10 +0530

    urls

--- Online_Shop/urls.py
+++ Online_Shop/urls.py
@@ -19,5 +19,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Shop.urls')),
+    path('/',include('Shop.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
