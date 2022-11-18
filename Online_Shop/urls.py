68289801f18971d36b5931df5c9f7769ff912e26
Suman Kumar Bhartiya <skb08725@gmail.com>
2022-11-18 16:27:10 +0530

    added all required files to deploy on heroku

--- Online_Shop/urls.py
+++ Online_Shop/urls.py
@@ -21,3 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Shop.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
++ static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS[0])
