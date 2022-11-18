1a300c909683c3d5720fd84042c98029a7b2ae5e
Suman Kumar Bhartiya <skb08725@gmail.com>
2022-11-18 17:04:31 +0530

    debug in settings.py

--- Online_Shop/settings.py
+++ Online_Shop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v!pd!4fws@#523@t!(zzdrcm3(!ilmpvsfj@y7%(!s^guj#0l*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
