f9e6f1a83ba42f799c27ac77eb0fb3d8e1d432e7
Suman Kumar Bhartiya <skb08725@gmail.com>
2022-11-18 16:38:07 +0530

    settings.static  files url updated

--- Online_Shop/urls.py
+++ Online_Shop/urls.py
@@ -21,4 +21,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Shop.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS[0])
