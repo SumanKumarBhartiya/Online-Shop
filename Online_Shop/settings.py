18ba3fecd2dc6a349ccd4ac36b83c5a5e9d24945
Suman Kumar Bhartiya <101987217+SumanKumarBhartiya@users.noreply.github.com>
2022-11-19 10:29:44 +0530

    Update settings.py

--- Online_Shop/settings.py
+++ Online_Shop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from decouple import config
-SCRET_KEY=config("SECRET_KEY")
+SECRET_KEY=config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
